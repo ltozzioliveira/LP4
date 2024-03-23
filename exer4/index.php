bf73308534b14e4914a754a48a133f1250425c64
lucas <ltozzioliveira@gmail.com>
2024-03-23 12:22:43 -0300

    exercício 4

---
+++ exer4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 4</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        <label>Tabuada do: </label>
+        <input type="number" name="value">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
