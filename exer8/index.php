2f0e0b5e4511d8b012b12762aa564cd2e23adf9d
lucas <ltozzioliveira@gmail.com>
2024-04-05 22:22:40 -0300

    exer7 in progress

---
+++ exer8/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 8</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        
+    </form>
+</body>
+</html>
