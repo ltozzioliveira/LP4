6ab5d4854328df7701e212b366dc15b416730c91
lucas <ltozzioliveira@gmail.com>
2024-04-13 12:14:44 -0300

    Organização da pasta

--- exer1/exer1-resposta.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $valor = $_POST['valor'];
-    if ($valor > 0)
-        echo "Valor positivo";
-    elseif ($valor < 0)
-        echo "Valor negativo";
-    else echo "Valor é zero";
--- exer4/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $value = $_POST['value'];
-    echo"<h1>Tabuada do $value</h1>";
-    for($i =0; $i <=10; $i++){
-        $result = $value*$i;
-        echo"<h3>$value x $i = $result</h3>";
-    }
-?>
